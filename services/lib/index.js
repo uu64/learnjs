61f0ef55ab7d088484fe0016a728ac628244a638
uu64 <yusuke.t.0808@gmail.com>
2020-05-13 12:19:51 +0900

    add: Chapter6

--- services/lib/index.js
+++ services/lib/index.js
@@ -1,3 +1,56 @@
-exports.echo = function(json, context) {  
-  context.succeed(["Hello from the cloud! You sent " + JSON.stringify(json)]);
+var http = require('http');
+var AWS = require('aws-sdk');
+
+AWS.config.region = 'ap-northeast-1';
+
+var config = {
+  dynamoTableName: 'learnjs',
+};
+
+exports.dynamodb = new AWS.DynamoDB.DocumentClient();
+
+function reduceItems(memo, items) {
+  items.forEach(function(item) {
+    memo[item.answer] = (memo[item.answer] || 0) + 1;
+  });
+  return memo;
+}
+
+function byCount(e1, e2) {
+  return e2[0] - e1[0];
+}
+
+function filterItems(items) {
+  var values = [];
+  for (i in items) {
+    values.push([items[i], i]);
+  }
+  var topFive = {};
+  values.sort(byCount).slice(0, 5).forEach(function(e) {
+    topFive[e[1]] = e[0];
+  });
+  return topFive;
+}
+
+exports.popularAnswers = function(event, context, callback) {
+  var res ={
+    "statusCode": 200,
+    "headers": {
+        "Content-Type": "*/*"
+    }
+  };
+  exports.dynamodb.scan({
+    FilterExpression: "problemId = :problemId",
+    ExpressionAttributeValues: {
+      ":problemId": JSON.parse(event.body).problemNumber
+    },
+    TableName: config.dynamoTableName,
+  }, function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      res.body = JSON.stringify(filterItems(reduceItems({}, data.Items)));
+      callback(null, res);
+    }
+  });
 };
--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -1,16 +1,35 @@
 describe('lambda function', function() {
   var index = require('index');
   var context;
+  var callback;
 
   beforeEach(function() {
-    context = jasmine.createSpyObj('context', ['succeed']);
+    callback = jasmine.createSpy('callback');
+    context = jasmine.createSpyObj('context', ['succeed', 'fail']);
+    index.dynamodb = jasmine.createSpyObj('dynamo', ['scan']);
   });
 
-  describe('echo', function() {
-    it('returns a result', function() {
-      index.echo({}, context);
-      expected = ["Hello from the cloud! You sent {}"];
-      expect(context.succeed).toHaveBeenCalledWith(expected);
+  describe('popularAnswers', function() {
+    it('requests problems with the given problem number', function() {
+      index.popularAnswers({ body: '{ "problemNumber": 42 }' }, context, callback);
+      expect(index.dynamodb.scan).toHaveBeenCalledWith({
+        FilterExpression: "problemId = :problemId",
+        ExpressionAttributeValues: { ":problemId": 42 },
+        TableName: 'learnjs'
+      }, jasmine.any(Function));
     });
+  });
+
+  it('groups answers by minified code', function() {
+    index.popularAnswers({ body: '{ "problemNumber": 42 }' }, context, callback);
+    index.dynamodb.scan.calls.first().args[1](undefined, {
+      Items: [
+        { answer: "true" },
+        { answer: "true" },
+        { answer: "true" },
+        { answer: "!false" },
+        { answer: "!false" },
+      ]
+    })
   });
 });
